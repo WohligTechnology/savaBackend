37463bb4b8c0c59c2fdece8a41bd06ab09b559b3
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2016-10-27 12:02:33 +0530

    Done

--- js/app.js
+++ js/app.js
@@ -130,20 +130,20 @@ firstapp.directive('onlyDigits', function() {
 });
 
 
-firstapp.directive('latlong', function() {
+firstapp.directive('latLong', function() {
     return {
         require: 'ngModel',
         restrict: 'A',
         link: function(scope, element, attr, ctrl) {
             function inputValue(val) {
                 if (val) {
-                    var digits = val.replace(/[^0-9]/g, '');
+                    var digits = val.replace(/[^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$]/g, '');
 
                     if (digits !== val) {
                         ctrl.$setViewValue(digits);
                         ctrl.$render();
                     }
-                    return parseInt(digits, 10);
+                    return digits;
                 }
                 return undefined;
             }
